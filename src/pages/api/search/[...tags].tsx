6f6d2e7ad1c493b290ff8fde2b8574357ac17ab0
unreact-hatano <mizutama.hahaha@gmail.com>
2022-09-23 17:50:44 +0900

    75%

--- src/pages/api/search/[...tags].tsx
+++
@@ -1,9 +0,0 @@
-// import { useRouter } from "next/router";
-
-// export default async (req, res) => {
-//   const tags = req.query.tags;
-//   const result = await searchTags(tags);
-//   res.status(200).json({
-//     result,
-//   });
-// };
