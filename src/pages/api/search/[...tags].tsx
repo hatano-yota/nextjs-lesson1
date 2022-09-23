db50013a1c016bd6ad97bfa13d413fd870642d82
unreact-hatano <mizutama.hahaha@gmail.com>
2022-09-23 17:49:33 +0900

    74%

--- src/pages/api/search/[...tags].tsx
+++ src/pages/api/search/[...tags].tsx
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-export default async (req, res) => {
-  const tags = req.query.tags;
-  const result = await searchTags(tags);
-  res.status(200).json({
-    result,
-  });
-};
+// export default async (req, res) => {
+//   const tags = req.query.tags;
+//   const result = await searchTags(tags);
+//   res.status(200).json({
+//     result,
+//   });
+// };
